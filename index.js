cb4b51a476aeea7d2ce67ac8765bcfe12a6374f5
bora <bora@boraui-MacBookPro.local>
2021-04-19 17:12:38 +0900

    index js move

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./src/App";
 
 ReactDOM.render(<App />, document.getElementById("root")); //App 을 public/index.html 의 root 라는 id를 가진 element 자식으로 추가함.
