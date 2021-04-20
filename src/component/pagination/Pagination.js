e92206e471dcf1576ee2cc8e1f00c06e74d60e3c
bora <bora@boraui-MacBookPro.local>
2021-04-20 14:43:02 +0900

    페이징바 css

--- src/component/pagination/Pagination.js
+++ src/component/pagination/Pagination.js
@@ -6,14 +6,14 @@ export default function Pagination(props) {
     <div className="pagination">
       <ul pagination="pagination">
         {props.pageNumbers.map((number) => (
-          <li
-            key={number}
-            className={
-              number == props.currentPageNumber ? "current_page" : "page"
-            }
-            onClick={() => props.paginate(number)}
-          >
-            {number}
+          <li key={number} onClick={() => props.paginate(number)}>
+            <span
+              className={
+                number == props.currentPageNumber ? "current_page" : "page"
+              }
+            >
+              {number}
+            </span>
           </li>
         ))}
       </ul>
