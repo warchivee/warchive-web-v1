950b05946b47db6a4c27db54c3d5223d4ec894a2
Jin Bora <55897222+borajin@users.noreply.github.com>
2021-12-24 22:45:54 +0900

    Update App.js
    wata 6 변경

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
   //==================== variable ====================
 
   //local storage name
-  const PAST_VIRSION_WATAS = "watas3";
-  const CURRENT_VERSION_WATAS = "watas4";
+  const PAST_VIRSION_WATAS = "watas4";
+  const CURRENT_VERSION_WATAS = "watas5";
   const BOOKMARK_LIST = "bookmarks";
 
   //state - wata
