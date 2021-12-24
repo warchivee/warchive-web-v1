7fdcfdc7d9b31fa9e8b5be91a50f2d6b1497d137
borajin <borajin.soft@gmail.com>
2021-12-24 23:18:03 +0900

    서버점검공지

--- src/component/loader/Loader.js
+++ src/component/loader/Loader.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Loader.css";
 
 export default function Loader() {
-  return <div className="loader">Loading...</div>;
+  return <div className="loader">{`서버 점검중입니다. 불편을 드려 죄송합니다.\n점검시간: 2012.12.24 23:00 ~ 2012.12.25 5:00`}</div>;
 }
